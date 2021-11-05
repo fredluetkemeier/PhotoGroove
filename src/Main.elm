7d1b2fe191d42fe80f4deb0cb7b99d2772983fa0
Fred Luetkemeier <fluetkemeier@midwayusa.com>
2021-11-05 15:30:47 -0500

    Start of SPA

---
+++ src/Main.elm
@@ -0,0 +1,80 @@
+module Main exposing (main)
+
+import Browser exposing (Document)
+import Html exposing (..)
+import Html.Attributes exposing (classList, href)
+import Html.Lazy exposing (lazy)
+
+
+type alias Model =
+    { page : Page }
+
+
+type Page
+    = Gallery
+    | Folders
+    | NotFound
+
+
+view : Model -> Document Msg
+view model =
+    let
+        content =
+            text "This isn't even my final form!"
+    in
+    { title = "Photo Groove, SPA Style"
+    , body =
+        [ lazy viewHeader model.page
+        , content
+        , viewFooter
+        ]
+    }
+
+
+viewHeader : Page -> Html Msg
+viewHeader page =
+    let
+        logo =
+            h1 [] [ text "Photo Groove" ]
+
+        links =
+            ul []
+                [ navLink Folders { url = "/folders", caption = "Folders" }
+                , navLink Gallery { url = "/gallery", caption = "Gallery" }
+                ]
+
+        navLink : Page -> { url : String, caption : String } -> Html msg
+        navLink targetPage { url, caption } =
+            li [ classList [ ( "active", page == targetPage ) ] ]
+                [ a [ href url ] [ text caption ] ]
+    in
+    nav [] [ logo, links ]
+
+
+viewFooter : Html msg
+viewFooter =
+    footer [] [ text "One is never alone with a rubber duck. -Douglas Adams" ]
+
+
+type Msg
+    = NothingYet
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+main : Program () Model Msg
+main =
+    Browser.document
+        { init = \_ -> ( { page = Gallery }, Cmd.none )
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
